76e5b4966b132197ee3a00f735b232d4d9f3fbb6
benjith17 <benjyabbott@gmail.com>
2026-06-04 21:24:25 +0100

    please work

--- .github/scripts/build-mac.sh
+++ .github/scripts/build-mac.sh
@@ -1,6 +1,6 @@
 # Build macOS (intel)
-GOOS=darwin GOARCH=amd64 go build -o ./build/mcsm-osx-intel.app/Contents/MacOS/mcsm-osx-intel .
+GOOS=darwin GOARCH=amd64 go build -o ./build/mcsm-osx-intel .
 
 
 # Build macOS (arm64)
-GOOS=darwin GOARCH=arm64 go build -o ./build/mcsm-osx-arm.app/Contents/MacOS/mcsm-osx-arm .
+GOOS=darwin GOARCH=arm64 go build -o ./build/mcsm-osx-arm .
--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,8 @@
 mkdir build
 
 for file in $(find .github/scripts -name "build-*.sh" -maxdepth 1 -type f); do
+  echo "Running $file..."
   sh "$file"
+  echo "Finished $file."
+  echo; echo
 done
